8cb06592a4c7f6b07c7b8698349b11b22a81b318
nasser <nasser.traoret@vimso.bf>
2021-11-19 10:04:29 +0000

    presentation

--- src/app/store/contrat/police/service.ts
+++ src/app/store/contrat/police/service.ts
@@ -1,5 +1,5 @@
 import { from } from "rxjs";
-import {Police, PoliceList} from "./model";
+import {Police, PoliceList, Report, Statistique} from "./model";
 import { HttpClient, HttpEvent, HttpRequest, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError, Observable} from 'rxjs';
@@ -9,64 +9,68 @@ import {Endpoints} from '../../../config/module.endpoints';
 
 @Injectable({providedIn: 'root'})
 export class PoliceService {
-constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-$getPolices(): Observable<PoliceList> {
-    // @FIXME: get request
-    return this.http.get( `${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}`).pipe(
-      map((response: PoliceList) => response),
-      catchError(this.handleError())
-    );
-  }
-
-posPolice(Police: Police): Observable<any> {
-    // @FIXME: post request
-    return this.http.post(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}`, Police);
-  }
-
-updatePolice(Police: Police): Observable<any> {
-    // @FIXME: post request
-    return this.http.put(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/${Police.id}`, Police);
-  }
-
-  validerPolice(Police: Police): Observable<any> {
-    // @FIXME: post request
-    return this.http.put(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/valider`, Police);
-  }
-
-deletePolice(Police: Police): Observable<any> {
-    // @FIXME: post request
-    return this.http.patch(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/${Police.id}`, null);
-}
-
-deletePolices(police: Array<Police>): Observable<any> {
-    // @FIXME: post request
-    return this.http.patch(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}`, police);
-}
-
-
-
-pushFileToStorage(file: File): Observable<any> {
-  const data: FormData = new FormData();
-  data.append('file', file);
-  let headers = new HttpHeaders();
-  headers.append('Content-Type', 'multipart/form-data');
-  headers.set('Accept', 'application/json');
-  return this.http.post(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/upload`, data, { headers: headers });
-}
-
-private handleError<T>() {
-    return (error: HttpErrorResponse) => {
-      return throwError(error.message || 'Something went wrong');
-    };
-  }
-
-    getPolicesFilterByAffNou(): Observable<PoliceList> {
+    $getPolices(): Observable<PoliceList> {
         // @FIXME: get request
-        return this.http.get( `${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/filter_by_affaire_nouvelle`).pipe(
+        return this.http.get( `${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}`).pipe(
             map((response: PoliceList) => response),
             catchError(this.handleError())
         );
     }
-}
 
+    $getStatistiquePolice(): Observable<Statistique> {
+        // @FIXME: get request
+        return this.http.get( `${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/statistique`).pipe(
+            map((response: Statistique) => response),
+            catchError(this.handleError())
+        );
+    }
+
+    $getReport(report: Report): Observable<ArrayBuffer> {
+        // @FIXME: get request
+        return this.http.post( `${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/report`,report, {responseType: 'arraybuffer'});
+    }
+
+    posPolice(Police: Police): Observable<any> {
+        // @FIXME: post request
+        return this.http.post(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}`, Police);
+    }
+
+    updatePolice(Police: Police): Observable<any> {
+        // @FIXME: post request
+        return this.http.put(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/${Police.id}`, Police);
+    }
+
+    validerPolice(Police: Police): Observable<any> {
+        // @FIXME: post request
+        return this.http.put(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/valider`, Police);
+    }
+
+    deletePolice(Police: Police): Observable<any> {
+        // @FIXME: post request
+        return this.http.patch(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/${Police.id}`, null);
+    }
+
+    deletePolices(police: Array<Police>): Observable<any> {
+        // @FIXME: post request
+        return this.http.patch(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}`, police);
+    }
+
+
+
+    pushFileToStorage(file: File): Observable<any> {
+        const data: FormData = new FormData();
+        data.append('file', file);
+        let headers = new HttpHeaders();
+        headers.append('Content-Type', 'multipart/form-data');
+        headers.set('Accept', 'application/json');
+        return this.http.post(`${GlobalConfig.getEndpoint(Endpoints.CONTRAT_POLICE)}/upload`, data, { headers: headers });
+    }
+
+    private handleError<T>() {
+        return (error: HttpErrorResponse) => {
+            return throwError(error.message || 'Something went wrong');
+        };
+    }
+}
