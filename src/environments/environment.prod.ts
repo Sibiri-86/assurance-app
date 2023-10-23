c181953bbcb0b21460059f365829a1b9d9ae50b6
Sibiri-86 <dambrelaurent82@gmail.com>
2023-10-23 11:18:00 +0000

    maj

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   keycloakConfig: {
-    url: 'https://192.168.59.252:28443/auth',
+    url: 'https://185.31.41.162:28443/auth',
     realm: 'Vimso',
     clientId: 'assurance-app'
   }
