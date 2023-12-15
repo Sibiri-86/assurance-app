00fa21c1a4ca672a7b345270dfc0d35f1beb4f7c
Sibiri-86 <dambrelaurent82@gmail.com>
2023-12-15 13:49:37 +0000

    maj

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   keycloakConfig: {
     //url: 'https://207.180.232.114/:28443/auth',
-    //url: 'https://178.170.14.201:28443/auth',
-    url: 'https://192.168.59.252:28443/auth',
+    url: 'https://solmavi.com:28443/auth',
+   // url: 'https://192.168.59.252:28443/auth',
     realm: 'Vimso',
     clientId: 'assurance-app'
   }
