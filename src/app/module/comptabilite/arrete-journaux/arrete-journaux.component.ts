4ad8d40560b743056d07c339b64759f283c658ba
nasser <nasser.traoret@vimso.bf>
2024-02-19 12:08:54 +0000

    maj

--- src/app/module/comptabilite/arrete-journaux/arrete-journaux.component.ts
+++ src/app/module/comptabilite/arrete-journaux/arrete-journaux.component.ts
@@ -335,12 +335,19 @@ export class ArreteJournauxComponent implements OnInit, OnDestroy {
     this.destroy$.unsubscribe();
   }
 
+  addMessage(severite: string, resume: string, detaile: string): void {
+    this.messageService.add({severity: severite, summary: resume, detail: detaile});
+  }
+
   supprimerEcriture(operation: Operation) {
     console.log("operation  ====>", operation);
     console.log("this.journal.id  ====>", this.journal.id);
     this.operationService.deleteOperationAndMajOther(operation, this.journal.id).subscribe((res)=> {
       if(res) {
-        this.operationList = res.body;
+        //this.operationList = res.body;
+        this.findJournalier();
+        this.addMessage('success', 'Effectué',
+      'Opération supprimée avec succès');
       }
     });
   }
