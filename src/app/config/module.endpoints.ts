7a883459f6ecbe27206d7dc16308ccba635cd2fa
Sibiri-86 <dambrelaurent82@gmail.com>
2025-01-22 14:25:01 +0000

    added filter nom on prenom

--- src/app/config/module.endpoints.ts
+++ src/app/config/module.endpoints.ts
@@ -530,7 +530,7 @@ export class Endpoints {
         };
 
         static readonly ADHERANT_BY_SOUSCRIPTEUR_BY_DATE_BY_PRENOM = {
-            prod: `${SERVICE_PREFIX}/contrat/adherent/getAssureBySouscripteur-adherent/paginate/prenom`,
+            prod: `${SERVICE_PREFIX}/contrat/adherent/getAssureBySouscripteur-adherent/paginate/nom-prenom`,
             mock: `assets/mock/contrat/adherent.mock.json`
         };
         static readonly ADHERANT_BY_SOUSCRIPTEUR_BY_DATE_BY_MATRICULE_GARANT = {
