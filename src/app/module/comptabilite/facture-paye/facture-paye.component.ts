e94aff7c61660d77da532707a6463a88792c411c
Sibiri-86 <dambrelaurent82@gmail.com>
2025-03-04 15:16:30 +0000

    added exportOrdreReglement

--- src/app/module/comptabilite/facture-paye/facture-paye.component.ts
+++ src/app/module/comptabilite/facture-paye/facture-paye.component.ts
@@ -212,10 +212,10 @@ export class FacturePayeComponent implements OnInit {
         this.messageService.add({severity: 'success', summary: 'PAIEMENT TIERS PAYANT', detail: 'Opération réussie!'});
       }
       getCancelInfo(): void {
-        this.messageService.add({severity: 'info', summary: 'PAIEMENT TIERS PAYANT', detail: 'Paiement annulé!'});
+        this.messageService.add({severity: 'info', summary: 'PAIEMENT TIERS PAYANT', detail: 'Opération annulé!'});
       }
       getFailledInfo(): void {
-        this.messageService.add({severity: 'error', summary: 'PAIEMENT TIERS PAYANT', detail: 'Paiement échouée!'});
+        this.messageService.add({severity: 'error', summary: 'PAIEMENT TIERS PAYANT', detail: 'Opération échouée!'});
       }
       
       getErrorInfo(message: string): void {
